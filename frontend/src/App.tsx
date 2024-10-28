c283e3db65d2c96a8e427747d1c7e979fbecdb10
Luka Koll <110080569+lkoll123@users.noreply.github.com>
2024-10-28 03:25:30 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,20 @@ import ColorPalette from "./components/ColorPalette";
 import Toolbar from "./components/Toolbar";
 
 function App() {
+  const usedColors: string[] = ["#FF1515", "#FF8000",
+    "#FFFF33", "#CCFF99", "#00FF00", "#33FFFF", "#99CCFF", 
+    "#0000FF", "#CC00CC", "#CC99FF", "#FF33FF", "#FF007F",
+    "#994C00", "#000000", "#FFFFFF" 
+  ]
+
+  const [currColor, setCurrColor] = useState<string>("#000000");
+
+  function setColor(arg: string) {
+    setCurrColor(arg);
+  }
   return (
     <>
-      <ColorPalette />
+      <ColorPalette colors={usedColors} setColor={setColor} />
       <Toolbar />
       <Canvas />
     </>
