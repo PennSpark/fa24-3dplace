06b51c2dcf2c8dd3356e7f21ce89ca33deb938e9
Luka Koll <110080569+lkoll123@users.noreply.github.com>
2024-10-28 03:21:03 -0400

    Update ColorPalette.tsx

--- frontend/src/components/ColorPalette.tsx
+++ frontend/src/components/ColorPalette.tsx
@@ -1,5 +1,19 @@
-function ColorPalette() {
-  return <></>;
+interface colorPalleteProps{
+  colors: string[]
+  setColor: (arg: string) => void
+}
+
+function ColorPalette({colors = [], setColor}: colorPalleteProps) {
+  console.log(colors);
+  return (<div className="color-pallete">
+      {colors.map((color) => {
+        return (<div
+          key={color} // Add a key for each element
+          onClick={() => setColor(color)} // Call setColor on click
+          style={{ backgroundColor: color, padding: '10px', margin: '5px', cursor: 'pointer' }}> </div>)
+      })}
+  </div>
+  );
 }
 
 export default ColorPalette;
