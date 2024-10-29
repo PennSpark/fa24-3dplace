45dab77b4d5ea60ae26208f4ca7c165981cc0783
Shaurya-Sarma <shaurya.sarma@gmail.com>
2024-10-29 16:16:09 -0400

    fix repo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,23 @@ import ColorPalette from "./components/ColorPalette";
 import Toolbar from "./components/Toolbar";
 
 function App() {
-  const usedColors: string[] = ["#FF1515", "#FF8000",
-    "#FFFF33", "#CCFF99", "#00FF00", "#33FFFF", "#99CCFF", 
-    "#0000FF", "#CC00CC", "#CC99FF", "#FF33FF", "#FF007F",
-    "#994C00", "#000000", "#FFFFFF" 
-  ]
+  const usedColors: string[] = [
+    "#FF1515",
+    "#FF8000",
+    "#FFFF33",
+    "#CCFF99",
+    "#00FF00",
+    "#33FFFF",
+    "#99CCFF",
+    "#0000FF",
+    "#CC00CC",
+    "#CC99FF",
+    "#FF33FF",
+    "#FF007F",
+    "#994C00",
+    "#000000",
+    "#FFFFFF",
+  ];
 
   const [currColor, setCurrColor] = useState<string>("#000000");
 
